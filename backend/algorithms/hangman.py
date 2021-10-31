4fb9ebb94dd0471ef33aa1588605691fb4a608cb
thinkvantagedu <duxiaohan2006@gmail.com>
2021-10-31 14:34:59 +0000

    Game logic done.

--- backend/algorithms/hangman.py
+++ backend/algorithms/hangman.py
@@ -3,7 +3,7 @@ import json, random
 
 is_play = None
 
-with open('../data/target_pool.json') as f:
+with open('../data/test_pool.json') as f:
     target_pool = json.load(f)
 
 pool_data = target_pool['targets']
@@ -17,19 +17,13 @@ print(f"Difficulty: {target_data['difficulty']}")
 
 word_list = target_data['words']
 
-for i in range(1, len(word_list)):
-
-    target = random.choice(word_list)
-
-    word_list = delete_the_guessed_word(target, word_list)
-
-    print(word_list)
-
-while is_play not in ('yes', 'no'):
+while is_play not in ('yes', 'no') and len(word_list) != 0:
     is_play = input('Start game? (y=Yes/n=No) ')
     if is_play == 'y':
 
-        target = random.choice(target_pool)
+        target = random.choice(word_list)
+
+        word_list = delete_the_guessed_word(target, word_list)
 
         iterations = len(target) - target.count(' ')
 
@@ -42,35 +36,35 @@ while is_play not in ('yes', 'no'):
         underlines_remain = len(underlines)
 
         while iterations > 0:
-        inp = input('Please type a letter in lower case: ')
-        guessed = list(set(guessed))
+            inp = input('Please type a letter in lower case: ')
+            guessed = list(set(guessed))
 
-        if underlines_remain == 0:
-            iterations = 0
-            print('Dead')
+            if underlines_remain == 0:
+                iterations = 0
+                print('Dead')
 
-        elif inp in pool:
-            underlines = user_guess_once(target, underlines, inp)
-            correct_num = underlines.count(inp)
-            iterations -= correct_num
-            pool.remove(inp)
-            guessed.append(inp)
-            print(underlines)
+            elif inp in pool:
+                underlines = user_guess_once(target, underlines, inp)
+                correct_num = underlines.count(inp)
+                iterations -= correct_num
+                pool.remove(inp)
+                guessed.append(inp)
+                print(underlines)
 
-        elif len(inp) > 1:
-            print('Only 1 letter allowed each time!')
+            elif len(inp) > 1:
+                print('Only 1 letter allowed each time!')
 
-        elif isinstance(inp, str) == False:
-            print('Only letters allowed!')
+            elif isinstance(inp, str) == False:
+                print('Only letters allowed!')
 
-        elif inp in guessed:
-            guessed.append(inp)
-            print('Already guessed!')
+            elif inp in guessed:
+                guessed.append(inp)
+                print('Already guessed!')
 
-        else:
-            guessed.append(inp)
-            underlines_remain -= 1
-            print(f'Closer to death, {underlines_remain} steps left')
+            else:
+                guessed.append(inp)
+                underlines_remain -= 1
+                print(f'Closer to death, {underlines_remain} steps left')
 
-elif is_play == 'n':
-    break
+    elif is_play == 'n':
+        break
