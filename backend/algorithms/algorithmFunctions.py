121921fb8c5a92a0cd246119ed3dd8a2bfad5384
Xiaohan Du <dux18@cardiff.ac.uk>
2021-11-11 19:33:03 +0000

    Send Keypress to BE, verify and return to FE

--- backend/algorithms/algorithmFunctions.py
+++ backend/algorithms/algorithmFunctions.py
@@ -25,3 +25,40 @@ def generate_pool(target):
 def delete_the_guessed_word(target, word_list):
     word_list.remove(target)
     return word_list
+
+
+
+
+
+
+
+
+# deleted methods
+
+def plot_dashes(target):
+    #  plot dashes based on {target} length, space is a space, target can be a phrase
+    #  @param {target} the input string
+    #  @return {dashes} a line of dashes, space is a space
+    target = target.split(' ')
+    dashes = ''
+    for item in target:
+        dashes = dashes + '-' * len(item) + ' '
+    return dashes[:-1]
+
+def get_categories(pool_data):
+    #  get words categories
+    #  @param {pool_data} the json data file content
+    #  @return {categories} list of categories
+    categories = []
+    for item in pool_data:
+        categories.append({'id': item['id'], 'category': item['category']})
+    return categories
+
+def get_category_details(pool_data, chosen_category):
+    #  return chosen category information from {pool_data}
+    #  @param {pool_data} the json data file content
+    #  @param {chosen_category} the chosen category name
+    #  @return {pool_data[candidate_id - 1]} the chosen category information
+    for data in pool_data:
+        if data['category'] == chosen_category:
+            return data
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from flask_cors import CORS #comment this on deployment
 from flask import Flask, request
-from algorithms import algorithmFunctions as af
 from algorithms.Hangman import Hangman
 import json
 
@@ -15,18 +14,30 @@ CORS(app) #comment this on deployment
 @app.route('/', methods = ['GET'])
 def home():
     if request.method == 'GET':
+        hangman.get_categories()
         return {
-            'categories': hangman.get_categories()
+            'categories': hangman.categories
         }
 
 @app.route('/<chosen_category_name>', methods = ['GET', 'POST'])
 def get_word_from_chosen_category(chosen_category_name):
-    if request.method == 'POST':
+    hangman.get_target(chosen_category_name)
+    if request.method == 'GET':
+        return f'Chosen category is {chosen_category_name}, target is {hangman.target}'
+    elif request.method == 'POST':
         # key_press = request.get_json(silent=True)['keyPress']
         chosen_category_name = request.get_json(silent=True)['chosenCategory']
-        target = hangman.get_target(chosen_category_name)
-        dashes = hangman.plot_dashes(target)
+        hangman.get_and_plot_dashes()
         return {
-            'dashes': dashes,
-            'target': target
+            'dashes': hangman.dashes,
+            'target': hangman.target
+        }
+
+@app.route('/keypress', methods = ['POST'])
+def verify_keypress():
+    if request.method == 'POST':
+        key_press = request.get_json(silent=True)['keyPress']
+        hangman.user_guess_once(key_press)
+        return {
+            'dashes': hangman.dashes
         }
